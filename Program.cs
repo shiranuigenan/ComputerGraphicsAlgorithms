3ed6b43ff582488a2ddc023259f43c9ed525e56a
q <q>
2020-12-25 14:11:24 +0300

    .

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 
 namespace ComputerGraphicsAlgorithms
@@ -7,8 +8,19 @@ namespace ComputerGraphicsAlgorithms
     {
         static void Main(string[] args)
         {
-            var pixels = imageGenerate.Gradient32();
-            var pixels64 = imageGenerate.Gradient64();
+            dithering.FloydSteinberg();
+            return;
+            var bmp = new Bitmap("1.png");
+
+            // var bmp2 = new Bitmap(bmp.Width, bmp.Height, PixelFormat.Format32bppArgb);
+            // using (Graphics gr = Graphics.FromImage(bmp2))
+            // {
+            //     gr.DrawImage(bmp, new Rectangle(0, 0, bmp2.Width, bmp2.Height));
+            // }
+            // bmp2.Save("1.png", ImageFormat.Png);
+
+            var byt = common.BitmapToByteArray(bmp);
+            var pixels = common.bytesToPixels(byt, bmp.Height, bmp.Width);
 
             var scaled = common.Scale16(pixels);
             var scaledBitmap = common.pixelsToBitmap(scaled);
