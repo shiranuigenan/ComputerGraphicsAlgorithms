df9e0eb4229cc2b492af2a03cc0f6611e2bbdb38
Kenan <q@q>
2023-08-10 15:50:56 +0300

    video generate düzenlemesi

--- Program.cs
+++ Program.cs
@@ -1,23 +1,3 @@
-﻿
-using ComputerGraphicsAlgorithms;
+﻿using ComputerGraphicsAlgorithms;
 
-var m = new Mp4Render(16384, 9216, 256, 60);
-m.OnFrameRender += (frameNumber, frameBuffer) =>
-{
-    var i = 0;
-    for (int h = 0; h < m.Height; h++)
-        for (int w = 0; w < m.Width; w++)
-        {
-            var r = 65535 - w - h - 256 * frameNumber;
-            var g = 45568 - w - h - 256 * frameNumber;
-            var b = 25600 - w - h - 256 * frameNumber;
-
-            frameBuffer[i++] = ((byte)r);
-            frameBuffer[i++] = ((byte)(r >> 8));
-            frameBuffer[i++] = ((byte)g);
-            frameBuffer[i++] = ((byte)(g >> 8));
-            frameBuffer[i++] = ((byte)b);
-            frameBuffer[i++] = ((byte)(b >> 8));
-        }
-};
-m.RenderToFile("2.mp4");
+VideoGenerate.Mp4RendererTest();
---
+++ VideoGenerate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ComputerGraphicsAlgorithms
+{
+    public static class VideoGenerate
+    {
+        public static void Mp4RendererTest()
+        {
+            var m = new Mp4Render(16384, 9216, 5, 60);
+            m.OnFrameRender += (frameNumber, frameBuffer) =>
+            {
+                var i = 0;
+                for (int h = 0; h < m.Height; h++)
+                    for (int w = 0; w < m.Width; w++)
+                    {
+                        var r = 65535 - w - h - 256 * frameNumber;
+                        var g = 45568 - w - h - 256 * frameNumber;
+                        var b = 25600 - w - h - 256 * frameNumber;
+
+                        frameBuffer[i++] = ((byte)r);
+                        frameBuffer[i++] = ((byte)(r >> 8));
+                        frameBuffer[i++] = ((byte)g);
+                        frameBuffer[i++] = ((byte)(g >> 8));
+                        frameBuffer[i++] = ((byte)b);
+                        frameBuffer[i++] = ((byte)(b >> 8));
+                    }
+            };
+            m.RenderToFile("2.mp4");
+        }
+    }
+}
