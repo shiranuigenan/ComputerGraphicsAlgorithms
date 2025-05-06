b0fde28aa62337c894c9b00b06d265b37181abc1
Kenan <q@q.com>
2025-05-06 08:52:29 +0300

    format düzenlemesi

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿for(int i =0; i<1000; i++){
-Console.WriteLine("Merhaba"+i);
-System.Threading.Thread.Sleep(500);
+﻿for (int i = 0; i < 1000; i++)
+{
+    Console.WriteLine("Merhaba" + i);
+    System.Threading.Thread.Sleep(500);
 }
